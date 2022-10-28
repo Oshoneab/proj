790b571dcaa19d038675a002e15c5155b2aab67b
Oshoneab <oshoneabiye@gmail.com>
2022-10-28 11:21:20 +0100

    Add existing projects to Git

---
+++ songcrud/songcrud/songcrud/musicappp/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+# Create your models here.
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=250)
+    last_name = models.CharField(max_length=250)
+    age = models.IntegerField()
+
+    
+    def __str__(self):
+        return self.first_name
+
+class Song(models.Model):
+    Artiste = models.ForeignKey(Artiste, on_delete = models.CASCADE)
+    title = models.CharField(max_length=250)
+    date_released = models.DateField(auto_now=False)
+    likes = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+    
+
+
+class Lyric(models.Model):
+    Song = models.ForeignKey(Song, on_delete = models.CASCADE)
+    content = models.TextField(max_length=20000)
+
+    
+    def __str__(self):
+        return self.content[:5]
