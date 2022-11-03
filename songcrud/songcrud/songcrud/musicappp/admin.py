3154561a201b48e39d2399790f8bcef3d0ec6d34
Oshoneab <oshoneabiye@gmail.com>
2022-11-03 09:52:03 +0000

    zuri task

--- songcrud/songcrud/songcrud/musicappp/admin.py
+++ songcrud/songcrud/songcrud/musicappp/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from . models import *
 
+class ArtisteAdmin(admin.ModelAdmin):
+    list_display = ['first_name','last_name','age','id']
+class SongAdmin(admin.ModelAdmin):
+    list_display = ['title', 'artiste_id','date_released','likes','id']
+class LyricAdmin(admin.ModelAdmin):
+    list_display = ['content','song_id']
 # Register your models here.
-admin.site.register(Artiste)
-admin.site.register(Song)
-admin.site.register(Lyric)
+admin.site.register(Artiste, ArtisteAdmin)
+admin.site.register(Song, SongAdmin)
+admin.site.register(Lyric, LyricAdmin)
